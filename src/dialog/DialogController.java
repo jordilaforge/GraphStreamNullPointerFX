a973ace8ac2d95c2dcb706e733fca6c24651d04c
jordilaforge <ph.m.steiner@gmail.com>
2016-10-26 12:20:05 +0200

    working project with null Pointer

--- src/dialog/DialogController.java
+++ src/dialog/DialogController.java
@@ -3,6 +3,7 @@ package dialog;
 import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.DefaultGraph;
@@ -20,6 +21,8 @@ public class DialogController extends BorderPane implements Initializable {
     @FXML
     private StackPane stackPane;
 
+    private Viewer viewer;
+
 
     public void initialize(URL location, ResourceBundle resources) {
         Graph graph = new DefaultGraph("TestGraph");
@@ -29,10 +32,16 @@ public class DialogController extends BorderPane implements Initializable {
         graph.addEdge("AB", "A", "B");
         graph.addEdge("AC", "A", "C");
         graph.addEdge("BC", "B", "C");
-        Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
-        ViewPanel defaultView = viewer.getDefaultView();
+        viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
+        ViewPanel defaultView = viewer.addDefaultView(false);
+        viewer.enableAutoLayout();
         SwingNode swingNode = new SwingNode();
         swingNode.setContent(defaultView);
         stackPane.getChildren().add(swingNode);
+        System.out.println("graph build");
+    }
+
+    public void closeViewer() {
+        viewer.close();
     }
 }
--- src/main/MainController.java
+++ src/main/MainController.java
@@ -1,11 +1,15 @@
 package main;
 
+import dialog.DialogController;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -18,9 +22,19 @@ public class MainController {
     public void dialogOpenButtonPressed() throws IOException {
         System.out.println("button pressed");
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("../dialog/Dialog.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Pane p = fxmlLoader.load(getClass().getResource("../dialog/Dialog.fxml").openStream());
+        final DialogController dialogController = (DialogController) fxmlLoader.getController();
+        final Parent root = FXMLLoader.load(getClass().getResource("../dialog/Dialog.fxml"));
         stage.setTitle("Add new entry");
         stage.setScene(new Scene(root, 800, 600));
         stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                dialogController.closeViewer();
+            }
+        });
     }
+
+
 }
